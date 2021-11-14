f52c279345058e59bec36bf4c6d0cdf880ce083a
zengxing358 <42179180+zengxing358@users.noreply.github.com>
2021-11-14 18:59:33 +0800

    Update Coordsutils.py

--- utils/Coordsutils.py
+++ utils/Coordsutils.py
@@ -6,3 +6,8 @@ def webMercator2lonlat(x,y):
     lonlat[0] = lon 
     lonlat[1] = lat 
     return lonlat
+def webMercatorToLonlat(x,y):
+    lon = x/20037508.34*180
+    lat = y/20037508.34*180 
+    lat= 180/np.pi*(2*np.arctan(np.exp(lat*np.pi/180))-np.pi/2)
+    return lon,lat
